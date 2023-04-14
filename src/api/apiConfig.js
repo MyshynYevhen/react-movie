4ea1e78c9de37a692d1ae33e3fd8f87ec8718ebc
MyshynYevhen <educationforfree2019to2023@gmail.com>
2023-04-14 18:40:32 +0300

    movie app

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb.org',
+    apiKey: 'a199a106734f36541a97c89e908b102a',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
